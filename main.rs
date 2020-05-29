8a9f43be40498ade723642ee4b0e1393d849798b
Tania Andersen <57521857+tania-andersen@users.noreply.github.com>
2020-05-29 16:11:34 +0200

    Update main.rs

--- main.rs
+++ main.rs
@@ -65,3 +65,74 @@ fn main() -> io::Result<()> {
     csv_wtr.flush()?;
     Ok(())
 }
+
+#[test]
+fn simple_usecase_test() {
+    let here = env::current_dir();
+    let here = match here {
+        Ok(dir) => dir,
+        Err(error) => panic!("Cannot get dir: {:?}", error),
+    };
+    println!("You are here: {:?}", here);
+    let copy_ok = fs::copy("target\\debug\\extract2csv.exe", "tests\\extract2csv.exe");
+    match copy_ok {
+        Ok(_) => (),
+        Err(error) => panic!("Cannot copy program to test folder: {:?}", error),
+    }
+    let cd_ok = env::set_current_dir("tests");
+    match cd_ok {
+        Ok(_) => (),
+        Err(error) => panic!("Cannot change directory to tests folder: {:?}", error),
+    }
+    let output = std::process::Command::new(".\\extract2csv.exe")
+        .arg("Tidspunkt for indberetningen*Referencenummer")
+        .arg("virksomhedens navn*Afdeling")
+        .arg("Beskriv hændelsen*Hvor fandt hændelsen fysisk sted")
+        .output();
+    match output {
+        Ok(_) => (),
+        Err(error) => panic!("Cannot run program: {:?}", error),
+    };
+    let file_content = fs::read_to_string(DEFAULT_OUT_FILENAME);
+    let file_content = match file_content {
+        Ok(file_content) => file_content,
+        Err(error) => panic!("Cannot open output file: {:?}", error),
+    };
+    assert!(
+        file_content.contains("2019-442-2007.PDF.txt"),
+        "Missing field"
+    );
+    assert!(
+        file_content.contains("onsdag november 6, 2019 14:14:01"),
+        "Missing field"
+    );
+    assert!(file_content.contains("Kalundborg kommune"), "Missing field");
+    assert!(
+        file_content.contains("hvis denne kode er 3"),
+        "Missing field"
+    );
+    assert!(
+        file_content.contains("torsdag november 7, 2019 09:32:32"),
+        "Missing field"
+    );
+    assert!(
+        file_content.contains("fredag november 15, 2019 08:27:28"),
+        "Missing field"
+    );
+    assert!(
+        file_content.contains("fredag november 8, 2019 11:56:09"),
+        "Missing field"
+    );
+    assert!(
+        file_content.contains("fredag november 8, 2019 13:08:49"),
+        "Missing field"
+    );
+    assert!(
+        file_content.contains("koden betyder, at billedet ikke må vises"),
+        "Missing field"
+    );
+    assert!(
+        file_content.contains("Kun ændring i antal berørte"),
+        "Missing field"
+    );
+}
